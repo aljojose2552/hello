8662735d7c18f1f9d75ba84fb40b72361798e25e
aljojose2552 <aljojose2552@gmail.com>
2024-09-27 14:11:19 +0100

    push

--- euler.js
+++ euler.js
@@ -2,3 +2,17 @@ alert("loaded euler JS");
 a=3;
 b=5;
 n=1000;
+
+<script>
+function calculateSum() 
+        {
+            let sum = 0;
+            for (let i = 0; i < 1000; i++)
+             {
+                if (i % 3 == 0 || i % 5 == 0) 
+                {
+                    sum += i;
+                }
+            }
+        }
+</script>
