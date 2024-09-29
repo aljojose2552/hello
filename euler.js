42571dec70e3d9953f0bebec9a9cf1b7c0fade01
aljojose2552 <aljojose2552@gmail.com>
2024-09-29 17:50:00 +0100

    kk

--- euler.js
+++ euler.js
@@ -8,7 +8,7 @@ function sumofmultiple(a,b,n)
 
 let sum = 0;   
            
-            for (let i = 0; i < 1000; i++)
+            for (let i = 0; i < n; i++)
              {
                 if (i%3 == 0 || i%5 == 0) 
                 {
