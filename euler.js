b0585d0895d0801f45314ebdb34dfeca2638cb05
aljojose2552 <aljojose2552@gmail.com>
2024-09-30 09:45:18 +0100

    oo

--- euler.js
+++ euler.js
@@ -5,13 +5,12 @@ let n=1000; */
 
 function calculate()
  {
-    // Get user inputs
+  
     let a = parseInt(document.getElementById("a").value);
     let b = parseInt(document.getElementById("b").value);
     let n = parseInt(document.getElementById("n").value);
     let sum = 0;
 
-    // Loop through numbers below n
     for (let i = 1; i < n; i++) 
         {
         if (i % a === 0 || i % b === 0) 
@@ -20,6 +19,5 @@ function calculate()
         }
     }
 
-    // Show the result
     alert("The sum of multiples of " + a + " or " + b + " below " + n + " is: " + sum);
 }
