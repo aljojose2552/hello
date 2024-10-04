91b1d2b6732f755f50de02311d427ec8d2941e3c
aljojose2552 <aljojose2552@gmail.com>
2024-10-04 14:22:21 +0100

    changes

--- euler1.js
+++ euler1.js
@@ -9,7 +9,7 @@ function calculateSum() {
         }
        // document.getElementById('result').textContent = 'The sum of multiples of numbers ${a} or ${b} below ${n} is: ${sum}';
 
-       // alert("The sum of multiples of numbers ${a} or ${b} below ${n} is: ${sum}");
+        alert("The sum of multiples of numbers ${a} or ${b} below ${n} is: ${sum}");
       
     }
     return sum;   
