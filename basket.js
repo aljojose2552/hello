f948551e5033989ccc2245ace2b0d5cff8e478f0
aljojose2552 <aljojose2552@gmail.com>
2024-10-11 12:24:21 +0100

    product add

--- basket.js
+++ basket.js
@@ -8,14 +8,14 @@ and prices is a mapping from products(String) to prices (number)
 and returns the total cost of the basket
 */
 let Basket = (basket,prices)=>{
-                let totalCost = 0;
+                let total = 0;
                 for(let item in basket){
                     if(prices[item] != undefined){
-                        totalCost += basket[item] * prices[item];
+                        total += basket[item] * prices[item];
                     }
                 }
-                return totalCost;
+                return total;
             }
-            basket = {apple:1,orange:1,grapes:5};
-            price = {apple:0.05,orange:1.5,grapes:2.0,tea:5,milk:10};
+            basket = {milk:1,Beaf:1,bread:2};
+            price = {apple:1.99,orange:1.05,grapes:1.80,bread:1.09,milk:2.09,Beaf:4.88};
             alert(Basket(basket,price));
